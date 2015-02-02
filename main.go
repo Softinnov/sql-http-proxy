b01dab74fc7a71cbc4176db22aeb7eb84a16ac98
Ghislain Guiot <gt.ghislain@gmail.com>
2015-02-02 11:49:51 +0100

    move api to query + add exec query

--- main.go
+++ main.go
@@ -18,9 +18,10 @@ var (
 )
 
 type QueryResult struct {
-	Columns []string    `json:"columns,omitempty"`
-	Data    [][]*string `json:"data,omitempty"`
-	Error   string      `json:"error,omitempty"`
+	Columns []string         `json:"columns,omitempty"`
+	Data    [][]*string      `json:"data,omitempty"`
+	Infos   map[string]int64 `json:"infos,omitempty"`
+	Error   string           `json:"error,omitempty"`
 }
 
 func WriteToJSON(w http.ResponseWriter, s int, v interface{}) error {
@@ -78,6 +79,60 @@ func HandleQuery(w http.ResponseWriter, r *http.Request) {
 		log.Println(e)
 	}
 	log.Printf("(Query rendered in %v)\n", time.Now().Sub(n))
+}
+
+func HandleExec(w http.ResponseWriter, r *http.Request) {
+	qr := &QueryResult{}
+
+	n := time.Now()
+
+	dbn := mux.Vars(r)["db"]
+	query := mux.Vars(r)["query"]
+
+	log.Printf("[DATABASE] %q [QUERY] %q\n", dbn, query)
+
+	s := qr.fetchExec(dbn, query)
+	e := WriteToJSON(w, s, qr)
+	if e != nil {
+		log.Println(e)
+	}
+	log.Printf("(Exec rendered in %v)\n", time.Now().Sub(n))
+}
+
+func (qr *QueryResult) fetchExec(dbn string, query string) int {
+	db, e := sql.Open(*flagDriver, "root:@/"+dbn)
+	if e != nil {
+		log.Fatal(e)
+	}
+	e = db.Ping()
+	if e != nil {
+		log.Fatal(e)
+	}
+	rs, e := db.Exec(query)
+	if e != nil {
+		log.Println(e)
+		qr.Error = fmt.Sprintf("%s", e)
+		return http.StatusBadRequest
+	}
+
+	li, e := rs.LastInsertId()
+	if e != nil {
+		log.Println(e)
+		qr.Error = fmt.Sprintf("%s", e)
+		return http.StatusBadRequest
+	}
+	ra, e := rs.RowsAffected()
+	if e != nil {
+		log.Println(e)
+		qr.Error = fmt.Sprintf("%s", e)
+		return http.StatusBadRequest
+	}
+
+	qr.Infos = map[string]int64{
+		"lastInsertId": li,
+		"rowsAffected": ra,
+	}
+	return http.StatusOK
 }
 
 func (qr *QueryResult) fetchQuery(dbn string, query string) int {
@@ -140,7 +195,8 @@ func main() {
 	flag.Parse()
 
 	m := mux.NewRouter()
-	m.HandleFunc("/api/{db}/{query}", HandleQuery).Methods("POST")
+	m.HandleFunc("/query/{db}/{query}", HandleQuery).Methods("POST")
+	m.HandleFunc("/exec/{db}/{query}", HandleExec).Methods("POST")
 	m.HandleFunc("/ping/{db}", HandlePing).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":6033", m))
